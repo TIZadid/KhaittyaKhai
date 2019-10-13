cd874bf579445018a8d06fdf584d6dc4eba8fc12
tizadid <talhazadid@gmail.com>
2019-10-13 23:58:17 +0600

    first commit

---
+++ app/src/main/java/i/t/khaittyakhai/MainActivity.java
@@ -0,0 +1,26 @@
+package i.t.khaittyakhai;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+ImageView BtnFinger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BtnFinger = findViewById(R.id.BtnFinger);
+        BtnFinger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/i/t/khaittyakhai/registerActivity.java
@@ -0,0 +1,13 @@
+package i.t.khaittyakhai;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class registerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
